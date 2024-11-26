f74561d04d8a031ed96a2c7d3775706f8b9dbff9
Valentun2 <pocomaxawrkt@gmail.com>
2024-11-26 10:42:09 +0200

    Update AICard.js

--- src/components/list_ai/AICard.js
+++ src/components/list_ai/AICard.js
@@ -2,7 +2,7 @@ const Card = ({ title, img, view, tag, arr, children }) => {
   return (
     arr?.includes(title.toLowerCase()) && (
       <li className="group w-[358px] md:w-[344px] lg:w-[288px] xl:w-[284px] 2xl:w-[404px] max-h-[297px] bg-cardsTransparent box-border border-[1px] border-cardsBorder flex flex-col  justify-between p-4 rounded-2xl hover:bg-cardsHover transition-all duration-500 relative">
-        <div className="absolute flex gap-2 right-5 pointer-events-none opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-500">
+        <div className="absolute flex items-center gap-2 right-5 pointer-events-none opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-500">
           <p className=" text-cardsText text-[16px]  ">Start chat</p>
           <svg
             width={16}
@@ -12,7 +12,7 @@ const Card = ({ title, img, view, tag, arr, children }) => {
             <use
               width={12}
               height={12}
-              href="./image/icons.svg##icon-right"
+              href="./image/icons.svg#icon-right"
             ></use>
           </svg>
         </div>
